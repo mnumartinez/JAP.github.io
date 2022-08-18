f629d56d5581669cac9eaf891652cc097dfa1411
Manuel Martinez <110356350+mnumartinez@users.noreply.github.com>
2022-08-18 19:41:55 -0300

    Update login.js

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function validar(){
+var email = document.getElementById("mail").value;
+var contraseña = document.getElementById("contraseña").value;
+if (email === "" || contraseña === ""){
+   return false;
+    }
+} 
