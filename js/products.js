ea2f5dfac05418193d6031c31d396ea0be8c5f58
Manuel Martinez <110356350+mnumartinez@users.noreply.github.com>
2022-08-21 15:57:21 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,18 +1,40 @@
 const url =  ("https://japceibal.github.io/emercado-api/cats_products/101.json");
 let cont = document.getElementById("auto");
+
+function interfaz (dataN){
+    cont.innerHTML = `<div class="text-center p-4">
+    <h2>Productos</h2>
+    <p class="lead">Veras aquí todos los productos de la categoría ${dataN}</p>
+    </div>`
+}
 function showData(dataN){
     for(let a of dataN){
-        cont.innerHTML += `<p class="mb-1">${a.name}</p>
-        <p>${a.description}</p>
-        <img src="${a.image}">
-            `
-    
- }
+        cont.innerHTML += `
+        <div class="row">
+            <div class="list-group">
+                <div class="list-group-item list-group-item-action cursor-active">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="img-thumbnail" src="${a.image}">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">${a.name} -${a.currency} ${a.cost}</h4>
+                                <small class="text-muted">${a.soldCount}</small>
+                            </div>
+                            <p class="mb-1">${a.description}</p>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>     
+`
+    }
 }
-
 async function fetchdata(){
     let response = await fetch(url);
     let data = await response.json();
+    interfaz(data.catName);
     showData(data.products);
 }
 fetchdata();
