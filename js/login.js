ceca70270fba82c67e8121cd99292617d4861d82
Manuel Martinez <110356350+mnumartinez@users.noreply.github.com>
2022-09-02 16:52:52 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,7 +3,11 @@ function validar(){
     var contraseña = document.getElementById("contraseña").value;
     if(email=== "" || contraseña === ""){
         alert("complete todos los campos");
+        
     }else{
         window.location = "mainPage.html"
     }
 }
+mail.addEventListener("focusout", function(){
+   localStorage.setItem("correo", mail.value);
+})
