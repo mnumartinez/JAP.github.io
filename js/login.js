f0f15884ac891379acb2952363b7d0a4139adb80
Manuel Martinez <110356350+mnumartinez@users.noreply.github.com>
2023-07-14 13:26:55 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,26 @@
-function validar() {
+function validar(){
     var email = document.getElementById("mail").value;
     var contraseña = document.getElementById("contraseña").value;
-    var emailRegExp = /\S+@\S+\.\S+/;
-    if (email === "" || contraseña.length < 7 || !emailRegExp.test(email)) {
-        alert("Complete todos los campos correctamente");
-    } else {
-        window.location = "mainPage.html";
+
+    if (!email || !contraseña) {
+        alert("complete ambos campos");
+        return false;
+    }   
+    if(email.length < 6 || email.length > 20 ){
+        alert("Ingrese un mail válido");
+        return false;
+    }
+    if (contraseña.length < 6 || contraseña.length > 20 ) {
+        alert("Ingrese una contraseña válida");
+        return false;
+    }
+    if (email.indexOf('@') === -1 || email.indexOf('.') === -1) {
+        alert("El mail debe contener '@' y un '.'");    
+        return false;
+    }else{
+        window.location = "mainPage.html"
     }
 }
+mail.addEventListener("focusout", function(){
+   localStorage.setItem("correo", mail.value);
+})
